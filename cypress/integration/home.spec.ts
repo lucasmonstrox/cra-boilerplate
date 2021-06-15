0369aa3e99c88be702674824b702c954f8a802ea
William Gonçalves <williamsg@taghos.com.br>
2021-06-15 19:15:50 -0300

    ✅ add error tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -24,3 +24,26 @@ it('should remove the task', () => {
   cy.get('[data-testid="remove-task-btn"').click();
   cy.get('[data-testid="task-item"]').should('not.exist');
 });
+
+it('should not add a task with empty field', () => {
+  cy.get('[data-testid="add-task-input"] input').clear();
+  cy.get('[data-testid="add-task-btn"]').click();
+  cy.get('[data-testid="task-item"]').should('not.exist');
+  cy.get('[data-testid="add-task-input"').contains('Entrada inválida (digite de 6 a 20 caracteres)');
+});
+
+it('should not add a task with less than 6 characters', () => {
+  cy.get('[data-testid="add-task-input"]').type('abcde');
+  cy.get('[data-testid="add-task-btn"]').click();
+  cy.get('[data-testid="task-item"]').should('not.exist');
+  cy.get('[data-testid="add-task-input"').contains('Entrada inválida (digite de 6 a 20 caracteres)');
+});
+
+
+it('should not add a task with more than 20 characters', () => {
+  cy.get('[data-testid="add-task-input"] input').clear();
+  cy.get('[data-testid="add-task-input"]').type('abcdeabcdeabcdeabcdea');
+  cy.get('[data-testid="add-task-btn"]').click();
+  cy.get('[data-testid="task-item"]').should('not.exist');
+  cy.get('[data-testid="add-task-input"').contains('Entrada inválida (digite de 6 a 20 caracteres)');
+});

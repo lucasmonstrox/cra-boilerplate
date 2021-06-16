3873846ef381c92be86d04420ca4adfde3148119
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-16 16:48:06 -0300

    :white_check_mark: Tests completed
    Tests completed

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -41,32 +41,27 @@ it('Should throw an error when the input is less than five characters', () => {
 
 it('Should generate an error when the input has more than ten characters', () => {
     cy.visit('/');
-    cy.get('[data-testid="taskTitle"]').type('12345678901234567890123');
+    cy.get('[data-testid="taskTitle"]').type('12345678901234567890123231231');
     cy.contains('Adicionar tarefa').click()
-    cy.contains('O máximo de caracter é 10')
+    cy.contains('O máximo de caracter é 20')
 })
 
+it('Should check if the task has added', () => {
+    cy.visit('/')
+    cy.get('[data-testid="taskTitle"]').type('Segunda');
+    cy.contains('Adicionar tarefa').click()
+    cy.contains('Tarefa adicionada com sucesso.')
+})
 
+it('Should cross off task when completed', () => {
+    cy.visit('/')
+    cy.get('[data-testid="btnChecked"]').click()
+    cy.get('[data-testid="textChecked"]').should('have.css', 'text-decoration-line', 'line-through')
+})
 
-
-// it('Should raise an error when the input is empty ', () => {
-//     cy.visit('/');
-//     cy.get('[data-testid="taskDescription"]').click()
-//     cy.contains('Adicionar tarefa').click()
-//     cy.contains('A descrição é obrigatória')
-// })
-
-// it('Should throw an error when the input is less than five characters', () => {
-//     cy.visit('/');
-//     cy.get('[data-testid="taskDescription"]').type('1234');
-//     cy.contains('Adicionar tarefa').click()
-//     cy.contains('O mínimo de caracter é 10')
-// })
-
-// it.only('Should generate an error when the input has more than ten characters', () => {
-//     cy.visit('/');
-//     cy.get('[data-testid="taskDescription"]').type('12345678901234567890123');
-//     cy.contains('Adicionar tarefa').click()
-//     cy.contains('O máximo de caracter é 20')
-// })
+it('Should check if the task has been removed', () => {
+    cy.visit('/')
+    cy.get('[data-testid="btnRemove"]').click()
+    cy.contains('Tarefa removida com sucesso.')
+})
 

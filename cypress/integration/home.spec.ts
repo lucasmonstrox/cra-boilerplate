1e6024bce0a5ef658852ca94b91d1d174da8de58
William Gonçalves <williamsg@taghos.com.br>
2021-06-16 13:06:02 -0300

    ✅ add selectors into functions

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,4 +1,11 @@
-const getTaskInput = () => cy.get('[data-testid="add-task-input"]');
+import {
+  getInput,
+  getInputField,
+  getAddButton,
+  getTodo,
+  getTodoRemoveButton,
+  getTodoToggleButton,
+} from '../support/po/home.po';
 
 it('should redirect to home page', () => {
   cy.visit('/');
@@ -6,46 +13,46 @@ it('should redirect to home page', () => {
 });
 
 it('should create a new task', () => {
-  getTaskInput().type('Created by Cypress');
-  cy.get('[data-testid="add-task-btn"]').click();
-  cy.get('[data-testid="task-item"]').its('length').should('eq', 1);
+  getInput().type('Created by Cypress');
+  getAddButton().click();
+  getTodo().its('length').should('eq', 1);
   cy.get('[data-testid="task-label"').contains('Created by Cypress');
 });
 
 it('should complete the task', () => {
-  cy.get('[data-testid="end-task-btn"').click();
-  cy.get('[data-testid="task-item"]').should('have.css', 'opacity', '0.5');
+  getTodoToggleButton().click();
+  getTodo().should('have.css', 'opacity', '0.5');
 });
 
 it('should reactivate the task', () => {
-  cy.get('[data-testid="end-task-btn"').click();
-  cy.get('[data-testid="task-item"]').should('have.css', 'opacity', '1');
+  getTodoToggleButton().click();
+  getTodo().should('have.css', 'opacity', '1');
 });
 
 it('should remove the task', () => {
-  cy.get('[data-testid="remove-task-btn"').click();
-  cy.get('[data-testid="task-item"]').should('not.exist');
+  getTodoRemoveButton().click();
+  getTodo().should('not.exist');
 });
 
 it('should not add a task with empty field', () => {
-  cy.get('[data-testid="add-task-input"] input').clear();
-  cy.get('[data-testid="add-task-btn"]').click();
-  cy.get('[data-testid="task-item"]').should('not.exist');
-  cy.get('[data-testid="add-task-input"').contains('Entrada inválida (digite de 6 a 20 caracteres)');
+  getInputField().clear();
+  getAddButton().click();
+  getTodo().should('not.exist');
+  getInput().contains('Entrada inválida (digite de 6 a 20 caracteres)');
 });
 
 it('should not add a task with less than 6 characters', () => {
-  getTaskInput().type('abcde');
-  cy.get('[data-testid="add-task-btn"]').click();
-  cy.get('[data-testid="task-item"]').should('not.exist');
-  cy.get('[data-testid="add-task-input"').contains('Entrada inválida (digite de 6 a 20 caracteres)');
+  getInput().type('abcde');
+  getAddButton().click();
+  getTodo().should('not.exist');
+  getInput().contains('Entrada inválida (digite de 6 a 20 caracteres)');
 });
 
 
 it('should not add a task with more than 20 characters', () => {
-  cy.get('[data-testid="add-task-input"] input').clear();
-  getTaskInput().type('abcdeabcdeabcdeabcdea');
-  cy.get('[data-testid="add-task-btn"]').click();
-  cy.get('[data-testid="task-item"]').should('not.exist');
-  cy.get('[data-testid="add-task-input"').contains('Entrada inválida (digite de 6 a 20 caracteres)');
+  getInputField().clear();
+  getInput().type('abcdeabcdeabcdeabcdea');
+  getAddButton().click();
+  getTodo().should('not.exist');
+  getInput().contains('Entrada inválida (digite de 6 a 20 caracteres)');
 });
---
+++ cypress/support/po/home.po.ts
@@ -0,0 +1,6 @@
+export const getInput = () => cy.get('[data-testid="add-task-input"]');
+export const getInputField = () => cy.get('[data-testid="add-task-input"] input');
+export const getAddButton = () => cy.get('[data-testid="add-task-btn"]');
+export const getTodo = () => cy.get('[data-testid="task-item"]');
+export const getTodoToggleButton = () => cy.get('[data-testid="end-task-btn"');
+export const getTodoRemoveButton = () => cy.get('[data-testid="remove-task-btn"');

814c4ea2f5ec66ced684af3378b1b820d020261c
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-16 16:59:40 -0300

    :rocket: Style inline and function to check the task
    Style inline and function to check the task

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,38 +1,57 @@
-import React, { FC } from 'react'
-import { ListItem, Checkbox, makeStyles, Box } from '@material-ui/core'
+import React, { FC, useState } from 'react'
+import {
+    Box,
+    Checkbox,
+    ListItem,
+    Typography
+} from '@material-ui/core'
 
 import DeleteIcon from '@material-ui/icons/Delete';
 
-const useStyles = makeStyles({
-    listItem: {
-        marginBottom: '.5rem'
-    },
-    containerIcon: {
-        marginLeft: 'auto',
-        cursor: 'pointer'
-    }
-})
-
 interface Props {
-    task: any
+    task: {
+        title: string,
+        taskId: number
+    },
+    onRemove: Function
 }
 
-const Task: FC<Props> = ({ task }: Props) => {
+const Task: FC<Props> = ({ task, onRemove }: Props) => {
 
+    const [taskChecked, setTaskChecked] = useState(false)
 
-    const classes = useStyles()
+    function toggleTaskChecked() {
+        setTaskChecked((prevState) => !prevState)
+    }
 
     return (
-    <ListItem classes={{ root: classes.listItem }}>
-        <Checkbox
-            inputProps={{ 'aria-label': 'primary checkbox' }}
-        />
-        <p>{task.title}</p>
-
-        <Box className={classes.containerIcon}>
-            <DeleteIcon />
-        </Box>
-    </ListItem>
-)}
+        <ListItem sx={{ mb: '.5rem' }} >
+            <Checkbox
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+                onChange={toggleTaskChecked}
+                data-testid="btnChecked"
+            />
+            <Typography
+                component="h4"
+                sx={{
+                    textDecoration: `${taskChecked && 'line-through'}`
+                }}
+                data-testid="textChecked"
+            >
+                {task.title}
+            </Typography>
+            <Box
+                sx={{
+                    ml: 'auto',
+                    cursor: 'pointer'
+                }}
+                onClick={() => onRemove(task.taskId)}
+                data-testid="btnRemove"
+            >
+                <DeleteIcon />
+            </Box>
+        </ListItem>
+    )
+}
 
 export default Task

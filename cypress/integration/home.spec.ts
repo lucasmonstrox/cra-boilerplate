7858093f8d0472c9be5556a0f5e00446d9fb4729
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-17 12:02:27 -0300

    ✅ Remove commented code

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,30 +1,3 @@
-// it('should redirect to home page', () => {
-//   cy.visit('/');
-//   cy.location('pathname').should('eq', '/');
-// });
-
-// it('Should appear task name error when form is submitted empty', () => {
-//     cy.visit('/');
-//     cy.get('[data-testid="submit"]').click()
-//     cy.get('[data-testid="taskNameError"]').should('be.visible')
-// })
-
-// it('Should create task when form is submitted correctly', () => {
-//     cy.visit('/');
-//     cy.get('[data-testid="task"]').type('Go to market')
-//     cy.get('[data-testid="submit"]').click()
-//     cy.contains('Go to market')
-// });
-
-// it('Should remove task when delete button is clicked', () => {
-//     cy.visit('/');
-//     cy.contains('Remover').click()
-//     cy.contains('Go to shopping').should('not.exist')
-// });
-
-
-/**/
-
 it('Should raise an error when the input is empty ', () => {
     cy.visit('/');
     cy.get('[data-testid="taskTitle"]').click()

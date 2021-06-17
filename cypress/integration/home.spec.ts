9710e0313004f422eb255fbe317af86457d6290a
William Gonçalves <williamsg@taghos.com.br>
2021-06-17 15:21:14 -0300

    ✅ refactor test for dynamic covering

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,51 +1,51 @@
 import {
-  getInput,
-  getInputField,
-  getAddButton,
-  getTodoItem,
-  getToggleTodoButton,
-  getRemoveTodoButton,
+  getInputs,
+  getInputsFields,
+  getAddButtons,
+  getTodoItems,
+  getToggleTodoButtons,
+  getRemoveTodoButtons,
 } from '../support/po/home.po';
 
 it('should not add a task when field is empty', () => {
   cy.visit('/');
-  getInputField().clear();
-  getAddButton().click();
-  getTodoItem().should('not.exist');
+  getInputsFields().clear();
+  getAddButtons().click();
+  getTodoItems().should('not.exist');
 });
 
 it('should not add a task when field has less than 6 characters', () => {
-  getInput().type('abcde');
-  getAddButton().click();
-  getTodoItem().should('not.exist');
+  getInputs().type('abcde');
+  getAddButtons().click();
+  getTodoItems().should('not.exist');
 });
 
 
 it('should not add a task when field has more than 20 characters', () => {
-  getInputField().clear();
-  getInput().type('abcdeabcdeabcdeabcdea');
-  getAddButton().click();
-  getTodoItem().should('not.exist');
+  getInputsFields().clear();
+  getInputs().type('abcdeabcdeabcdeabcdea');
+  getAddButtons().click();
+  getTodoItems().should('not.exist');
 });
 
 it('should create a new task when field is valid', () => {
-  getInputField().clear();
-  getInput().type('Created by Cypress');
-  getAddButton().click();
-  getTodoItem().should('be.visible');
+  getInputsFields().clear();
+  getInputs().type('Created by Cypress');
+  getAddButtons().click();
+  getTodoItems().should('be.visible');
 });
 
 it('should complete the task when it was created', () => {
-  getToggleTodoButton().click();
-  getTodoItem().should('have.css', 'opacity', '0.5');
+  getToggleTodoButtons().click();
+  getTodoItems().should('have.css', 'opacity', '0.5');
 });
 
 it('should reactivate the task when it was completed', () => {
-  getToggleTodoButton().click();
-  getTodoItem().should('have.css', 'opacity', '1');
+  getToggleTodoButtons().click();
+  getTodoItems().should('have.css', 'opacity', '1');
 });
 
 it('should remove the task when it was created', () => {
-  getRemoveTodoButton().click();
-  getTodoItem().should('not.exist');
+  getRemoveTodoButtons().click();
+  getTodoItems().should('not.exist');
 });
--- cypress/support/po/home.po.ts
+++ cypress/support/po/home.po.ts
@@ -1,6 +1,6 @@
-export const getInput = () => cy.get('[data-testid="add-todo-input"]');
-export const getInputField = () => cy.get('[data-testid="add-todo-input"] input');
-export const getAddButton = () => cy.get('[data-testid="add-todo-btn"]');
-export const getTodoItem = () => cy.get('[data-testid="todo-0"]');
-export const getToggleTodoButton = () => cy.get('[data-testid="toggle-todo-0"');
-export const getRemoveTodoButton = () => cy.get('[data-testid="remove-todo-0"');
+export const getInputs = () => cy.get('[data-testid^="add-todo-input"]');
+export const getInputsFields = () => cy.get('[data-testid^="add-todo-input"] input');
+export const getAddButtons = () => cy.get('[data-testid^="add-todo-btn"]');
+export const getTodoItems = () => cy.get('[data-testid^="todo-"]');
+export const getToggleTodoButtons = () => cy.get('[data-testid^="toggle-todo-"');
+export const getRemoveTodoButtons = () => cy.get('[data-testid^="remove-todo-"');

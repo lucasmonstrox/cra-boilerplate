e80267bdf433471c1161fe36ac5e8132d9f5d5a4
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-17 16:22:56 -0300

    ✅ Format the file

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { ApolloProvider } from '@apollo/client';
 import apiGraphql from './Services/GraphClient';
 import HomePage from './Components/HomePage';
 
-const App: FC = () =>  (
-    <ApolloProvider client={apiGraphql}>
-      <CssBaseline />
-      <HomePage />
-    </ApolloProvider>
-  )
+const App: FC = () => (
+  <ApolloProvider client={apiGraphql}>
+    <CssBaseline />
+    <HomePage />
+  </ApolloProvider>
+)
 export default App;

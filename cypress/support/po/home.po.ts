7e95b080b33a2282b0bdd3002c09d73bfa7e1c58
William Gonçalves <williamsg@taghos.com.br>
2021-06-17 09:24:12 -0300

    ✅ refactor form selectors

--- cypress/support/po/home.po.ts
+++ cypress/support/po/home.po.ts
@@ -1,6 +1,6 @@
-export const getInput = () => cy.get('[data-testid="add-task-input"]');
-export const getInputField = () => cy.get('[data-testid="add-task-input"] input');
-export const getAddButton = () => cy.get('[data-testid="add-task-btn"]');
+export const getInput = () => cy.get('[data-testid="add-todo-input"]');
+export const getInputField = () => cy.get('[data-testid="add-todo-input"] input');
+export const getAddButton = () => cy.get('[data-testid="add-todo-btn"]');
 export const getTodoItem = () => cy.get('[data-testid="todo-0"]');
 export const getToggleTodoButton = () => cy.get('[data-testid="toggle-todo-0"');
 export const getRemoveTodoButton = () => cy.get('[data-testid="remove-todo-0"');

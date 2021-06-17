ed2c24e77ded662b8419eafc0a74ed166d351f3c
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-17 18:28:30 -0300

    ✅ Using formik hooks

--- src/Components/TaskForm.tsx
+++ src/Components/TaskForm.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react'
-
 import {
     Box,
     Button,
     Container,
     InputLabel,
+    TextField,
 } from '@material-ui/core';
-
-import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik'
-
+import { FormikHelpers, useFormik } from 'formik'
 import schema from './Validation/schema';
 
 interface Props {
@@ -24,51 +22,52 @@ const TaskForm: FC<Props> = ({ onTaskAdded }: Props) => {
         onTaskAdded(values)
     }
 
+    const { handleSubmit, errors, handleChange, values, handleBlur } = useFormik({
+        initialValues: {
+            title: ""
+        },
+        onSubmit: handleSubmitAddTask,
+        validationSchema: schema
+    })
+
     return (
         <Container>
-            <Formik
-                initialValues={{
-                    title: ''
-                }}
-                onSubmit={handleSubmitAddTask}
-                validationSchema={schema}
-            >
-                {() => (
-                    <Form>
-                        <InputLabel
-                            sx={{ mb: '.3rem', mt: '1rem' }}
-                            htmlFor="titleTask">
-                            Título
-                        </InputLabel>
-                        <Field
-                            id="title"
-                            name="title"
-                            type="text"
-                            style={{ width: '100%', padding: '.5rem' }}
-                            data-testid="task-title-input"
-                        />
-                        <Box sx={{ color: 'red', mt: '.3rem' }} data-testid="title-msg-error">
-                            <ErrorMessage name="title" />
-                        </Box>
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                            fullWidth
-                            sx={{
-                                mt: 2
-                            }}
-                            type="submit"
-                            data-testid="add-task-button"
-                        >
-                            Adicionar tarefa
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
+            <form onSubmit={handleSubmit}>
+                <InputLabel
+                    sx={{ mb: '.3rem', mt: '1rem' }}
+                    htmlFor="titleTask">
+                    Título
+                </InputLabel>
+                <TextField
+                    id="title"
+                    name="title"
+                    type="text"
+                    style={{ width: '100%' }}
+                    data-testid="task-title-input"
+                    value={values.title}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                <Box sx={{ color: 'red', mt: '.3rem' }} data-testid="title-msg-error">
+                    {
+                        errors.title && <p>{errors.title}</p>
+                    }
+                </Box>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    fullWidth
+                    sx={{
+                        mt: 2
+                    }}
+                    type="submit"
+                    data-testid="add-task-button"
+                >
+                    Adicionar tarefa
+                </Button>
+            </form>
         </Container>
     )
 }
-
-
 
 export default TaskForm

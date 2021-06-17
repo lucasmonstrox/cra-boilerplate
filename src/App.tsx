4847a19625a6936d6f19fb8a68131f0027c5db0e
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-17 16:17:34 -0300

    ✅ Removed this line

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { CssBaseline } from '@material-ui/core';
-
 import { ApolloProvider } from '@apollo/client';
 import apiGraphql from './Services/GraphClient';
 import HomePage from './Components/HomePage';

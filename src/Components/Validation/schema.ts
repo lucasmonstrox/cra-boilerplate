911f1a4878955a747442dbb7b292beb69ff6658b
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-18 14:16:50 -0300

    :recycle: Change variables to global

--- src/Components/Validation/schema.ts
+++ src/Components/Validation/schema.ts
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
 
+const TITLE_MINLENGTH = 5;
+const TITLE_MAXLENGTH = 20;
+
 export default Yup.object().shape({
     title: Yup
         .string()
-        .min(5, 'O mínimo de caracter é 5')
-        .max(20, 'O máximo de caracter é 20')
+        .min(TITLE_MINLENGTH, `O mínimo de caracteres é ${TITLE_MINLENGTH}`)
+        .max(TITLE_MAXLENGTH, `O máximo de caracteres é ${TITLE_MAXLENGTH}`)
         .required('O título é obrigatório'),
 });

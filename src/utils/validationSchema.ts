4ec8a49263405cba56e704b18ae213e6f0c96634
lucasmonstro <joao.galiano.silva@gmail.com>
2021-06-18 21:04:23 +0100

    :recycle: rename form fields

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,13 +1,9 @@
 import * as Yup from 'yup';
-import {
-  MIN_TODO_LENGTH,
-  MAX_TODO_LENGTH
-} from './globalVariables';
+import { MIN_TODO_LENGTH, MAX_TODO_LENGTH } from './globalVariables';
 
 export default Yup.object().shape({
-  todoName: Yup
-  .string()
-  .min(MIN_TODO_LENGTH, `Digite ao menos ${MIN_TODO_LENGTH} caracteres`)
-  .max(MAX_TODO_LENGTH, `Digite até ${MAX_TODO_LENGTH} caracteres`)
-  .required('O campo não pode estar vazio'),
+  name: Yup.string()
+    .min(MIN_TODO_LENGTH, `Digite ao menos ${MIN_TODO_LENGTH} caracteres`)
+    .max(MAX_TODO_LENGTH, `Digite até ${MAX_TODO_LENGTH} caracteres`)
+    .required('O campo não pode estar vazio'),
 });

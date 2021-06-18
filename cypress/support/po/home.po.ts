345338e43c2736eb9c25ff6e789561aa21958088
lucasmonstro <joao.galiano.silva@gmail.com>
2021-06-18 21:29:38 +0100

    :recycle: improve testid

--- cypress/support/po/home.po.ts
+++ cypress/support/po/home.po.ts
@@ -1,4 +1,4 @@
 export const getTodoNameInput = () => cy.get('[data-testid="add-todo-input"]');
 export const getTodoNameError = () => getTodoNameInput().find('.Mui-error');
-export const getAddButton = () => cy.get('[data-testid^="add-todo-btn"]');
+export const getAddButton = () => cy.get('[data-testid="add-todo-button"]');
 export const getTodo = (id) => cy.get(`[data-testid="todo-${id}`);

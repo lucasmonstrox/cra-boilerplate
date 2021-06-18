8822130d17e099b6bff87869454154729c30079d
lucasmonstro <joao.galiano.silva@gmail.com>
2021-06-18 21:10:14 +0100

    :recycle: improve page objects

--- cypress/support/po/home.po.ts
+++ cypress/support/po/home.po.ts
@@ -1,6 +1,4 @@
-export const getInputs = () => cy.get('[data-testid^="add-todo-input"]');
-export const getInputsFields = () => cy.get('[data-testid^="add-todo-input"] input');
-export const getAddButtons = () => cy.get('[data-testid^="add-todo-btn"]');
-export const getTodoItems = () => cy.get('[data-testid^="todo-"]');
-export const getToggleTodoButtons = () => cy.get('[data-testid^="toggle-todo-"');
-export const getRemoveTodoButtons = () => cy.get('[data-testid^="remove-todo-"');
+export const getTodoNameInput = () => cy.get('[data-testid="add-todo-input"]');
+export const getTodoNameError = () => getTodoNameInput().find('.Mui-error');
+export const getAddButton = () => cy.get('[data-testid^="add-todo-btn"]');
+export const getTodo = (id) => cy.get(`[data-testid="todo-${id}`);

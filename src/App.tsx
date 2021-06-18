583001d167d9742054bd0cf9dd64e843bb5b8a7d
Marianna Lemos <marianalemos07@hotmail.com>
2021-06-18 13:42:38 -0300

    :recycle: Change to apolloClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { ApolloProvider } from '@apollo/client';
-import apiGraphql from './Services/GraphClient';
+import apolloClient from './helpers/apolloClient';
 import HomePage from './Components/HomePage';
 
 const App: FC = () => (
-  <ApolloProvider client={apiGraphql}>
+  <ApolloProvider client={apolloClient}>
     <CssBaseline />
     <HomePage />
   </ApolloProvider>
